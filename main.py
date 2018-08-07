be738da411573bf7d14cc14964c5eea159a18cd4
misc77 <misc29377@gmail.com>
2018-08-07 23:52:31 +0200

    removed test code

--- main.py
+++ main.py
@@ -2,54 +2,6 @@ from docx import Document
 import xml.etree.ElementTree as ET
 from checklist import Checklist
 
-""" 
-    readTable
-
-    reads the content of a word table
-    and puts the content into an instance of checklist class.
-    This instance will be persisted into an Sqlite table
-"""
-def readTable(table):
-    r = c = 0
-    print("columns: " + str(len(table.columns)) + " rows: " + str(len(table.rows)))
-
-    for row in table.rows:
-        r=r+1
-        print("---- row " + str(r) + "---")
-        c=0
-        for col in table.columns:
-            c=c+1
-            cell = table.cell(r-1, c-1)
-            print(str(r) +"," + str(c))
-            print(cell.text)
-            if len(cell.tables) > 0:
-                for t in cell.tables:
-                    readTable(t)
-
-"""
-    readChecklist
-
-    reads the input document that is basis for
-    generation of the dse-document
-"""
-def readChecklist(file):
-    document = Document(file)
-    i = 0
-
-    for t in document.tables:
-        i=i+1
-        print("####### START (" + str(i) + ") #######")
-        readTable(t)
-        print("####### END (" + str(i) + ") #######")
-        print("")
-        if i>=5:
-            break
-
-    print("document finished - contained " + str(i) + " tables")
-
-
-"""readChecklist("C:/Users/misc2/Documents/PyProjects/DSEGenerator/test/data/checkliste.docx")"""
-
 def parseTable(xmlElement, checklistDocument):
     pos = int(xmlElement.attrib.get("tabelle"))
     table = checklistDocument.tables[pos-1]
